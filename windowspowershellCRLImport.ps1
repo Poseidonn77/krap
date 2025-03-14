be3717585ed7d9b8c26df9315bd5c5389fc1e8f4
Martin J de Klerk <martin@mjdk.io>
2025-03-14 09:37:16 +0200

    Update windowspowershellCRLImport.ps1

--- windowspowershellCRLImport.ps1
+++ windowspowershellCRLImport.ps1
@@ -1,28 +1,8 @@
-# Windows PowerShell Script to Download and Import CRLs
+# This PowerShell script downloads a list of CRLs and imports them into the Windows certificate store.
+# Be sure to run this as Administrator.
 
 # Array of CRL URLs
-$CRL_URLS = @(
-    "https://www.apple.com/appleca/root.crl",
-    "https://www.apple.com/certificateauthority/root.crl",
-    "https://crl.apple.com/softwareupdateca.crl",
-    "https://crl.apple.com/timestamp.crl",
-    "https://developer.apple.com/certificationauthority/wwdrca.crl",
-    "https://crl.apple.com/apsrsa12g1.crl",
-    "https://crl.apple.com/apsecc12g1.crl",
-    "https://crl.apple.com/appleistca2g1.crl",
-    "https://crl.apple.com/appleistca8g1.crl",
-    "https://crl.apple.com/apevsrsa1g1.crl",
-    "https://crl.apple.com/apevsrsa2g1.crl",
-    "https://crl.apple.com/apevsrsaca3g1.crl",
-    "https://crl.apple.com/apevsecc1g1.crl",
-    "https://crl.apple.com/aptrsa1g1.crl",
-    "https://crl.apple.com/aptecc1g1.crl",
-    "http://crl.emsign.com/RootCAG2.crl",
-    "http://crl.emsign.com/RootCAG3.crl",
-    "http://crl.emsign.com/RootCAC2.crl",
-    "http://crl.emsign.com/RootCAC3.crl",
-    "http://crl.emsign.com/rootcscag2.crl",
-    "http://crl.emsign.com/rootcscag3.crl",
+$crlUrls = @(
     "http://crl.usertrust.com/USERTrustRSACertificationAuthority.crl",
     "http://crl.usertrust.com/USERTrustECCCertificationAuthority.crl",
     "http://crl3.digicert.com/DigiCertTrustedRootG4.crl",
@@ -32,12 +12,12 @@ $CRL_URLS = @(
     "http://crl3.digicert.com/DigiCertAssuredIDRootCA.crl",
     "http://crl4.digicert.com/DigiCertAssuredIDRootCA.crl",
     "http://crl4.digicert.com/DigiCertHighAssuranceEVRootCA.crl",
-    "http://crl.sectigo.com/SectigoPublicCodeSigningCAR36.crl",
     "http://crls.ssl.com/ssl.com-rsa-RootCA.crl",
     "http://rootca2017-crl1.e-szigno.hu/rootca2017.crl",
     "http://rootca2017-crl2.e-szigno.hu/rootca2017.crl",
     "http://rootca2017-crl3.e-szigno.hu/rootca2017.crl",
     "http://crl.comodoca.com/COMODORSACertificationAuthority.crl",
+    "http://crl.sectigo.com/SectigoPublicCodeSigningCAR36.crl",
     "http://crl3.digicert.com/DigiCertAssuredIDRootG2.crl",
     "http://crl3.digicert.com/DigiCertCSRSA4096RootG5.crl",
     "http://crl.certum.pl/ctnca.crl",
@@ -52,7 +32,6 @@ $CRL_URLS = @(
     "http://rtcrl.cybertrust.ne.jp/SecureSign/rtca14/cdp.crl",
     "http://rtcrl.cybertrust.ne.jp/SecureSign/rtca15/cdp.crl",
     "http://validation.identrust.com/crl/commercialrootca1.crl",
-    "http://www.microsoft.com/pkiops/crl/MicCodSigPCA2011_2011-07-08.crl",
     "http://crl.emsign.com?RootCAG2.crl",
     "http://crl.emsign.com?RootCAG3.crl",
     "http://crl.entrust.net/g2ca.crl",
@@ -69,97 +48,46 @@ $CRL_URLS = @(
     "http://crl.emsign.com?RootCAC3.crl",
     "http://crl.entrust.net/csbr1.crl",
     "http://crl.global.sheca.com/root/evg1.crl",
-    "http://crl.globalsign.com/gsgccr45codesignca2020.crl",
     "http://crl.harica.gr/HARICA-CodeSigning-Root-2021-ECC.crl",
     "http://crl.harica.gr/HARICA-CodeSigning-Root-2021-RSA.crl",
     "http://crl.sectigo.com/SectigoPublicCodeSigningRootE46.crl",
     "http://crl.startssl.com/sfsca.crl",
-    "http://crl.wt.trustasia.com/TrustAsiaGlobalRootCAG3.crl",
-    "http://crl.wt.trustasia.com/TrustAsiaGlobalRootCAG4.crl",
-    "http://crl05.actalis.it/Repository/AUTH-ROOT/getLastCRL",
-    "http://crls.ssl.com/SSL.com-Enterprise-Intermediate-codeSigning-RSA-4096-R1.crl",
-    "http://crls.ssl.com/ssl.com-EVecc-RootCA.crl",
-    "http://crls.ssl.com/SSLcom-CodeSigning-Root-2022-RSA.crl",
-    "http://crls.ssl.com/SSLcom-RootCA-ECC-384-R1.crl",
-    "http://crls.ssl.com/SSLcom-RootCA-EV-ECC-384-R1.crl",
-    "http://crls.ssl.com/SSLcom-RootCA-RSA-4096-R1.crl",
-    "http://crls.ssl.com/SSLcom-SubCA-CodeSigning-RSA-4096-R1.crl",
-    "http://crls.ssl.com/ssl.com-EVrsa-RootCA.crl",
-    "http://g.symcb.com/GeoTrustPCA.crl",
-    "http://pki-crl.atos.net/crl/Atos_TrustedRoot_CA_2011.crl",
-    "http://repository.secomtrust.net/SC-Root3/SCRoot3CRL.crl",
-    "http://s.symcb.com/pca3-g5.crl",
-    "http://s.symcb.com/universal-root.crl",
-    "http://t.symcb.com/ThawtePCA-G3.crl",
-    "http://validation.identrust.com/crl/trustidevcodesigning4.crl",
-    "http://www.anf.es/crl/ANF_Global_Root_CA_SHA1.crl",
-    "http://www.apple.com/appleca/root.crl",
-    "http://www.microsoft.com/pkiops/crl/Microsoft%20ECC%20Product%20Root%20Certificate%20Authority%202018.crl",
-    "http://www.microsoft.com/pkiops/crl/Microsoft%20Windows%20Third%20Party%20Component%20CA%202012.crl"
+    "http://crl.wt.trustasia.com/TrustAsiaGlobalRootCAG3.crl"
 )
 
 # Temporary directory to store downloaded CRLs
-$TEMP_DIR = "$env:TEMP\apple_crls"
+$tempDir = Join-Path $env:TEMP "apple_crls"
 
-# Create the temporary directory
-New-Item -ItemType Directory -Path $TEMP_DIR -Force | Out-Null
-
-# Function to Validate URL Format
-function Validate-CRLUrl {
-    param (
-        [string]$url
-    )
-
-    # Check if the URL starts with http:// or https://
-    if ($url -notmatch "^https?://") {
-        return $false
-    }
-
-    # Basic URI validation
-    try {
-        $uri = [System.Uri]$url
-        return $true
-    }
-    catch {
-        return $false
-    }
+# Create the temporary directory if it doesn't exist
+if (!(Test-Path -Path $tempDir)) {
+    New-Item -ItemType Directory -Path $tempDir | Out-Null
 }
 
-# Loop over the CRL URLs
-foreach ($CRL_URL in $CRL_URLS) {
-    # Validate the URL
-    if (-not (Validate-CRLUrl -url $CRL_URL)) {
-        Write-Warning "Invalid URL format: $CRL_URL. Skipping..."
-        continue
-    }
-
+foreach ($url in $crlUrls) {
     # Extract the filename from the URL
-    $FILENAME = [System.IO.Path]::GetFileName($CRL_URL.Split('?')[0])  # Remove query parameters if any
-    $CRL_FILE = Join-Path $TEMP_DIR $FILENAME
+    $filename = Split-Path -Path $url -Leaf
+    $crlFile = Join-Path -Path $tempDir -ChildPath $filename
 
-    # Download the CRL
-    Write-Output "Downloading CRL from $CRL_URL..."
+    Write-Host "Downloading CRL from $url..."
     try {
-        Invoke-WebRequest -Uri $CRL_URL -OutFile $CRL_FILE -ErrorAction Stop
+        Invoke-WebRequest -Uri $url -OutFile $crlFile -ErrorAction Stop
     }
     catch {
-        Write-Warning "Failed to download the CRL from $CRL_URL. Error: $_"
+        Write-Host "Failed to download the CRL from $url."
         continue
     }
 
-    # Import the CRL into the system certificate store
-    Write-Output "Importing the CRL from $FILENAME into the system certificate store..."
-    try {
-        # Use certutil to add CRL to the 'CA' store
-        certutil -addstore -f "CA" $CRL_FILE | Out-Null
-    }
-    catch {
-        Write-Warning "Failed to import the CRL from $FILENAME. Error: $_"
+    Write-Host "Importing the CRL from $filename into the system certificate store..."
+    # Import the CRL using certutil. (Store "ROOT" is chosen here.)
+    & certutil -addstore -f "ROOT" $crlFile
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to import the CRL from $filename."
         continue
     }
 }
 
 # Clean up the temporary directory
-Remove-Item -Path $TEMP_DIR -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path $tempDir -Recurse -Force
 
-Write-Output "All CRLs have been processed."
+Write-Host "All CRLs have been processed."
